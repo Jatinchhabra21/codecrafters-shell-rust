affd732445c153bdb42e46bb1c5283d6e2bbd299
Jatin Chhabra <cjatin2822@gmail.com>
2024-12-08 16:31:05 +0530

    repl loop

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,10 @@
 use std::io::{self, Write};
 
 fn main() {
+    let exit: bool = false;
     // Uncomment this block to pass the first stage
+    while !exit {
+        
     print!("$ ");
     io::stdout().flush().unwrap();
 
@@ -12,4 +15,5 @@ fn main() {
     stdin.read_line(&mut input).unwrap();
     
     println!("{}: not found", input.trim());
+    }
 }
